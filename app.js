d86efbf8197238b63614539810fb1d627ba442e3
phuong090802 <20110547@student.hcmute.edu.vn>
2023-11-13 22:48:20 +0700

    Update

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ import cors from 'cors';
 
 const app = express();
 const corsOptions = {
-    origin: process.env.FRONTEND_URL,
+    origin: 'http://localhost:5173',
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
