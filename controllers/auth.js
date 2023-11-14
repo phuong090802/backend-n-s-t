e52b21ecb20e575fc3292586e72134909129d572
phuong090802 <20110547@student.hcmute.edu.vn>
2023-11-14 11:15:18 +0700

    Update

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,13 +2,13 @@ import User from '../models/user.js';
 import catchAsyncError from '../middlewares/catchAsyncErrors.js'
 
 export const registerUser = catchAsyncError(async (req, res, next) => {
-    const { name, email, phone, password } = req.body;
-    await User.create({
-        name,
-        email,
-        phone,
-        password,
-    });
+    // const { name, email, phone, password } = req.body;
+    // await User.create({
+    //     name,
+    //     email,
+    //     phone,
+    //     password,
+    // });
 
     res.json({
         success: true,
