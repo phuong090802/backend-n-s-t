218c7e909915da6d56e6c6b3b36468e6f5126f1b
phuong090802 <20110547@student.hcmute.edu.vn>
2023-11-15 23:13:13 +0700

    Update

--- controllers/auth.js
+++ controllers/auth.js
@@ -97,8 +97,10 @@ export const handleRefreshToken = catchAsyncError(async (req, res, next) => {
     const user = await User.findById(refreshToken.user);
     if (!refreshToken.hasOwnProperty('parent')) {
         await RefreshToken.findByIdAndUpdate(refreshToken._id, { status: false });
-        await RefreshToken.deleteMany({ parent: refreshToken._id });
+
     }
+    await RefreshToken.deleteMany({ parent });
+
     const nextRefreshToken = await getNextRefreshToken(user._id, parent);
     sendToken(user, nextRefreshToken, res);
 });
