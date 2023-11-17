5222bce5139d2465381bc5db23f398332b02b3d7
phuong090802 <20110547@student.hcmute.edu.vn>
2023-11-17 21:53:36 +0700

    Update

--- controllers/user.js
+++ controllers/user.js
@@ -1,6 +1,7 @@
 import User from '../models/user.js';
 import catchAsyncError from '../middlewares/catchAsyncErrors.js';
 import { v2 as cloudinary } from 'cloudinary';
+import ErrorHandler from '../utils/errorHandler.js';
 
 
 export const handleUpdateProfile = catchAsyncError(async (req, res, next) => {
@@ -27,5 +28,35 @@ export const handleUpdateProfile = catchAsyncError(async (req, res, next) => {
     res.json({
         success: true,
         message: 'Cập nhật thông tin thành công'
+    })
+});
+
+export const handleUpdateEmail = catchAsyncError(async (req, res, next) => {
+
+    await User.findByIdAndUpdate(req.user.id, req.body.email, {
+        runValidators: true,
+    });
+
+    res.json({
+        success: true,
+        message: 'Cập nhật email thành công'
+    })
+});
+
+export const handleUpdatePassword = catchAsyncError(async (req, res, next) => {
+    const user = await User.findById(req.user.id).select('+password');
+
+    const isPasswordMatched = await user.comparePassword(password);
+    if (!isPasswordMatched) {
+        return next(new ErrorHandler('Mật khẩu cũ không chính xác', 400));
+    }
+    
+    user.password = req.body.password;
+
+    await user.save();
+
+    res.json({
+        success: true,
+        message: 'Cập nhật mật khẩu thành công'
     })
 });
