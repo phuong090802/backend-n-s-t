d83f1ac1a0704d4fbd4c68970066e22f306c11e2
phuong090802 <20110547@student.hcmute.edu.vn>
2023-11-20 22:19:56 +0700

    Update

--- utils/tokenUtils.js
+++ utils/tokenUtils.js
@@ -26,7 +26,10 @@ export const sendToken = (user, refreshToken, res) => {
 }
 
 export const clearToken = (res) => {
-    res.clearCookie('refreshToken', { path: '/api/v1/auth' });
+    res.cookie('token', null, {
+        expires: new Date(Date.now()),
+        httpOnly: true,
+    });
 }
 
 export const getRefreshToken = async (user) => {
